7cc126901bf549c8f3ee28c2026f8adfc06b67ff
Ludo <ludovic.saussinan@gmail.com>
2017-10-14 20:49:45 +0800

    auto layout done

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var swooshLogo: UIImageView!
+    @IBOutlet var backgroundImg: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
